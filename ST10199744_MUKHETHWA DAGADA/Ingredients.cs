06051b24a33392e134e26e0cf102c944304b819c
lab_services_student <ST10199744>
2023-05-29 16:56:59 +0000

    Added getters and setters

--- ST10199744_MUKHETHWA DAGADA/Ingredients.cs
+++ ST10199744_MUKHETHWA DAGADA/Ingredients.cs
@@ -9,16 +9,7 @@ namespace Dagada_Mukhethwa_ST10199744
         class Ingredientss
         {
 
-            public string Name { get; set; }
-            public double Quantity { get; set; }
-            public string Unit { get; set; }
-
-            public Ingredientss(string name, double quantity, string unit)
-            {
-                Name = name;
-                Quantity = quantity;
-                Unit = unit;
-            }
+         
         }
     }
 
